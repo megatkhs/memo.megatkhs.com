b97a38656151f213df392b0d1009535fc8f89d1e
megatkhs <ryo.takahashi@megatkhs.com>
2025-02-20 08:40:51 +0900

    PostCSSの設定を変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,22 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
-    require('postcss-preset-env', {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-      features: {
-        'custom-media-queries': {
-          preserve: false,
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
         },
-        'nesting-rules': true,
+        stage: 3,
+        features: {
+          'custom-media-queries': {
+            preserve: false,
+          },
+          'nesting-rules': true,
+        },
+        browsers: 'last 2 versions',
       },
-      browsers: 'last 2 versions',
-    }),
+    ],
   ],
 };
 
